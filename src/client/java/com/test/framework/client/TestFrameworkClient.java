de41cf307ec87a9a3560a0d402b4bdd9d1c7729f
Lucy Wood <lucywood325@outlook.com>
2026-09-19 15:56:12 +0100

    Add initial project template

---
+++ src/client/java/com/test/framework/client/TestFrameworkClient.java
@@ -0,0 +1,10 @@
+package com.test.framework.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestFrameworkClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
